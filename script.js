392acb83b3cafc745efc158f8a916310b0c0b1ea
odai2024 <odai2015100@gmail.com>
2024-09-15 18:56:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function changecolor(){
+document.getElementById("text").style.color="#A04747";
+}
+
+function changecolorbg(){
+document.getElementById("text2").style.backgroundColor="grey";
+}
+
+function changebackground(){
+document.body.style.backgroundColor = "blue";
+}
